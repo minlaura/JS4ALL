36037bac47ee69323bca98244c12f375fcd0cdc4
Min.Laura <laurarars@gmail.com>
2025-06-10 20:22:19 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,24 @@
+function mostrarSolucao(id) {
+  const solucao = document.getElementById(id);
+  if (solucao.style.display === "block") {
+    solucao.style.display = "none";
+  } else {
+    solucao.style.display = "block";
+  }
+}
+function mostrarSolucao(id) {
+  const solucao = document.getElementById(id);
+  if (solucao.style.display === "block") {
+    solucao.style.display = "none";
+  } else {
+    solucao.style.display = "block";
+  }
+}
+function mostrarSolucao(id) {
+  const solucao = document.getElementById(id);
+  if (solucao.style.display === "block") {
+    solucao.style.display = "none";
+  } else {
+    solucao.style.display = "block";
+  }
+}
